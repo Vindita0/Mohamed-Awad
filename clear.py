3b71aefe7ab9ff5b7652a6a1331ce7ecadca1811
OmegaCrimson <nightangelyt01@gmail.com>
2026-02-13 01:38:03 +0200

    refactor: add subprocess and Final from typing

--- clear.py
+++ clear.py
@@ -1,4 +1,16 @@
 import os
+import subprocess
+from typing import Final
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+IS_WINDOWS: Final[bool] = os.name == 'nt'
+CLEAR_CMD: Final[str] = 'cls' if IS_WINDOWS else 'clear'
+
+def clear() -> None:
+    """Clears terminal screen."""
+    try:
+        if IS_WINDOWS:
+            subprocess.run(CLEAR_CMD, shell=True, check=True)
+        else:
+            subprocess.run([CLEAR_CMD], check=True)
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("\n" * 100)
